32ac5fae174c038a479cabde0054f177dd730457
IcePhoenix09 <63466963+IcePhoenix09@users.noreply.github.com>
2020-09-18 16:26:55 +0300

    roma

--- Game3D.py
+++ Game3D.py
@@ -1,9 +1,11 @@
 import pygame
+import math
 
 run = True
 
 map_scale = 200
 map_lenght = 10
+real_map_size =  10 * map_lenght
 wall_lenght = map_scale / map_lenght
 map_background_color = 47, 79, 79
 map_list = '''##########
@@ -22,7 +24,7 @@ class Game:
     window_y = 400
     window = pygame.display.set_mode((window_x, window_y))
     @classmethod
-    def draw_minimap(cls, map_color, map_scale):
+    def draw_minimap(cls, map_color, map_scale, player):
         count_x = 0
         count_y = 0
         pygame.draw.rect(Game.window, (map_color), (Game.window_x - map_scale, 0, map_scale, map_scale))
@@ -33,6 +35,34 @@ class Game:
             if item == '#':
                 pygame.draw.rect(Game.window, (0, 0, 0),(count_x * wall_lenght + Game.window_x - map_scale, count_y * wall_lenght, wall_lenght, wall_lenght))
             count_x += 1
+        pygame.draw.circle(Game.window, (0, 0, 30), player.x, player.y, 3)
+
+class Player:
+    def __init__(self, x, y, degree, viewing_angle):
+        self.x = x
+        self.y = y
+        self.degree = degree
+        self.direction = conver_degree_in_vector(self.degree)
+        self.v_a =  viewing_angle
+    def move():
+        self.x += direction[0]
+        self.y += direction[1]
+
+    def rotate(degree):
+        self.dir = conver_degree_in_vector(self.degree + degree)
+
+class Wall:
+    def __init__(self, x, y, widgh, color):
+        self.x = x
+        self.y = y
+        self.widgh = widgh
+        self.color = color
+
+    def collision(pos):
+        if self.x < pos[0] < self.x + self.widgh and self.y < pos[1] < self.y + self.widgh:
+            return True
+def conver_degree_in_vector(degree):
+    return [math.sin(degree), math.cos(degree)]
 for item in map_list:
     print(item, end = '')
     if item != '0' and item != '#':
@@ -40,7 +70,7 @@ for item in map_list:
 while run:
     for e in pygame.event.get():
         if e == pygame.QUIT:
-            pygame.exit()
+            run = False
     Game.window.fill((119, 136, 153))
     Game.draw_minimap(map_background_color, map_scale)
     pygame.display.update()
